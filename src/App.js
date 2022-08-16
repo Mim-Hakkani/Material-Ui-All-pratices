9d34c5f38c33280337e6041f9ef8c159c9c6abcf
Hakkani Mim <mimhakkani4g@gmail.com>
2022-08-16 08:42:32 +0600

    box componet is finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Bismillah Hir Rahmanir Rahim </h1>
+    
+     <Box 
+     
+     sx={{
+      border:'1px solid red',
+      padding:'5px', 
+      // add hover in inline css 
+     '&:hover': {
+        background: "#f00",
+      }}} 
+
+      //  add the css style without sx 
+       fontSize="25px"
+       color='violet'
+       m="0 auto"
+       textTransform="uppercase"
+       textAlign='center'
+       display='flex'
+       flexDirection={'column'}
+    
+       >
+        <p>  1. it works as div by default </p>  
+        <p>  2. add all styles like as css </p>  
+        <p>  3. play with the box items   </p>  
+         
+          
+     </Box>
+   
     </div>
   );
 }
